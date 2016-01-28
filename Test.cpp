c6a3fe629a0fbf26c33f2a40a4e9ae791dea3390
mnnguy23 <michaelnguyen1105@gmail.com>
2016-01-28 08:22:38 -0600

    first commit, added test file. Only created headers for test file at the moment.

---
+++ Test.cpp
@@ -0,0 +1,11 @@
+#ifndef Test_h
+#define Test_h
+#include <iostream>
+#include <string>
+
+#include <cppunit/TestCase.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TestCaller.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+
